1b13384c1c6cfd9e93d71e3c5fa81f511c0c2709
DuongKhangAu <93584478+AuDuongKhang@users.noreply.github.com>
2024-07-03 21:43:54 +0700

    Feature: Authenticator (#3)
    * Feature: add log out for authenticator

--- trending_app.py
+++ trending_app.py
@@ -21,6 +21,8 @@ def main():
     name, authentication_status, username = authenticator.login()
 
     if authentication_status:
+        authenticator.logout()
+
         st.title("TikTokTechJam-CLOCK")
 
         tab1, tab2, tab3 = st.tabs(
@@ -138,13 +140,6 @@ def main():
         st.error('Username/password is incorrect')
     elif authentication_status == None:
         st.warning('Please enter your username and password')
-
-    # Optional: Add register and forgot password buttons
-    if st.sidebar.button('Register'):
-        authenticator.register_user('Register user', preauthorization=False)
-
-    if st.sidebar.button('Forgot password'):
-        authenticator.forgot_password('Reset password')
 
 
 if __name__ == "__main__":
