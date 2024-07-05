cc1c8e0bb666be694bfdfc651a3cd9849b3985e8
huy20 <51670533+huy20@users.noreply.github.com>
2024-07-05 15:13:30 +0700

    Update text_to_sound.py

--- text_to_sound.py
+++ text_to_sound.py
@@ -8,7 +8,7 @@ warnings.filterwarnings("ignore")
 processor = AutoProcessor.from_pretrained("facebook/musicgen-small")
 model = MusicgenForConditionalGeneration.from_pretrained("facebook/musicgen-small")
 
-def changeConfig(guidance_scale, max_new_tokens, temperature):
+def changeConfig(guidance_scale=4.0, max_new_tokens=256, temperature=1.5):
     model.generation_config.guidance_scale = guidance_scale
     model.generation_config.max_new_tokens = max_new_tokens
     model.generation_config.temperature = temperature
@@ -17,18 +17,14 @@ def changeConfig(guidance_scale, max_new_tokens, temperature):
 def getPrompt(prompt: str):
     return prompt.splitlines()
 
-def gen(out_name, prompt, max_new_token=512):
-
+def gen(out_name, prompt, max_token=256):
+    changeConfig()
     inputs = processor(
         text=getPrompt(prompt),
         padding=True,
         return_tensors="pt",
     )
 
-    audio_values = model.generate(**inputs, max_new_tokens=max_new_token)
+    audio_values = model.generate(**inputs, max_new_tokens=max_token)
     sampling_rate = model.config.audio_encoder.sampling_rate
-    scipy.io.wavfile.write(out_name + ".wav", rate=sampling_rate, data=audio_values[0, 0].numpy())
-
-prompt = 'Title: "The Mysterious Disappearance of Flight 19: Uncovering the Truth Behind the Bermuda Triangle\'s Most Infamous Case"'
-changeConfig(4.0, 256, 1.5)
-gen("gen", prompt)
+    scipy.io.wavfile.write(out_name, rate=sampling_rate, data=audio_values[0, 0].numpy())
